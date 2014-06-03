bf283bafdf31d9f93f883d87aeb3f567f57db1f2
Jacob Coble <c4bee386@opayq.com>
2014-06-03 10:53:00 -0400

    Create a new cookie when the session is stored.

--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -2,30 +2,38 @@ require 'json'
 require 'webrick'
 
 class Session
+
+  COOKIE_NAME = "_rails_light_app"
+
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
-    self.cookie = get_app_cookie(req)
+    cookie = get_app_cookie(req)
   end
 
   def [](key)
-    self.cookie[key]
+    cookie[key]
   end
 
   def []=(key, val)
-    self.cookie[key] = val
+    cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.cookies << new_app_cookie
   end
 
   private
   attr_accessor :cookie
 
   def get_app_cookie(req)
-    cookie = req.cookies.select { |cookie| cookie.name = "_rails_light_app"}
+    cookie = req.cookies.find { |cookie| cookie.name = COOKIE_NAME }
     cookie.nil? ? {} : JSON::parse(cookie.value)
+  end
+
+  def new_app_cookie
+    WEBrick::Cookie.new(COOKIE_NAME, self.cookie.to_json)
   end
 end
